844e66c57e2de7060fe86fe1c0994f697401aa25
srivalli-dasari <srivallidasari0706@gmail.com>
2026-01-13 09:17:51 -0500

    Convert LoginTest to TestNG

--- src/test/java/ui_tests/LoginTest.java
+++
@@ -1,5 +0,0 @@
-public class LoginTest {
-    public static void main(String[] args) {
-        System.out.println("AI Automation project setup started");
-    }
-}
---
+++ src/test/java/ui_tests/src/test/java/ui_tests/LoginTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void sampleLoginTest() {
+        System.out.println("TestNG test executed successfully");
+    }
+}
